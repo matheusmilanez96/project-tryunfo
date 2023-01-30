2862c5f9f0d414a2928cccbf589f3c308d717f18
Matheus Milanez <mattmila96@gmail.com>
2023-01-30 15:48:26 -0300

    requisito 3 proptypes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
   render() {
@@ -26,5 +27,16 @@ class Card extends React.Component {
     );
   }
 }
+
+Card.propTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+}.isRequired;
 
 export default Card;
